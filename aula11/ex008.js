5361c1be77b94b8ccd084542e1ee7f0d171760c4
Alexandre Akyra <110061213+AlexandreAkyra@users.noreply.github.com>
2024-10-23 12:22:37 -0300

    ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`Sua velocidade é de ${vel}Km/H`)
+if (vel > 60) { // Condição simples
+    console.log('Você foi multado por excesso de velocidade')
+}
+console.log('Dirija sempre com cinto de segurança')
